8007a891d2a8c0cf293ce899e4a2cf7714540da2
Jenn Jordan <1jordanjennifer@gmail.com>
2024-05-04 12:47:52 -0400

    Updated the LabSetUp script to include appropriate server VMs during deployment

--- LabSetUp.sh
+++ LabSetUp.sh
@@ -5,6 +5,7 @@ vnetName="SOCLabVNet"
 subnetName="SOCLabSubnet"
 nsgName="SOCLabNSG"
 vmSize="Standard_DS1_v2"
+ubuntuImage="Canonical:UbuntuServer:19_04-daily-gen2:19.04.202001230"
 
 # Read credentials from file
 readarray -t credentials < credentials.txt
@@ -26,9 +27,30 @@ az network nic create --resource-group $resourceGroupName --name "FileServerNIC"
 az network nic create --resource-group $resourceGroupName --name "EmployeeNIC" --vnet-name $vnetName --subnet $subnetName --network-security-group $nsgName
 az network nic create --resource-group $resourceGroupName --name "SIEMNIC" --vnet-name $vnetName --subnet $subnetName --network-security-group $nsgName
 
-# Create VMs with proper images
-az vm create --resource-group $resourceGroupName --name "FileServerVM" --image "UbuntuLTS::22.04-LTS:latest" --size $vmSize --admin-username $adminUsername --admin-password $adminPassword --nics "FileServerNIC"
+# Create Employee VM with proper images
 az vm create --resource-group $resourceGroupName --name "EmployeeVM" --image "Win2022Datacenter" --size $vmSize --admin-username $adminUsername --admin-password $adminPassword --nics "EmployeeNIC"
-az vm create --resource-group $resourceGroupName --name "SIEMServer" --image "UbuntuLTS::22.04-LTS:latest" --size $vmSize --admin-username $adminUsername --admin-password $adminPassword --nics "SIEMNIC"
+
+# Deploy VMs with proper images
+# Create File Server VM
+az vm create \
+  --resource-group $resourceGroupName \
+  --name FileServerVM \
+  --nics FileServerNIC \
+  --image $ubuntuImage \
+  --size $vmSize \
+  --admin-username $adminUsername \
+  --admin-password $adminPassword \
+  --no-wait
+
+# Create SIEM/SOAR VM
+az vm create \
+  --resource-group $resourceGroupName \
+  --name SIEMServer \
+  --nics SIEMNIC \
+  --image $ubuntuImage \
+  --size $vmSize \
+  --admin-username $adminUsername \
+  --admin-password $adminPassword \
+  --no-wait
 
  
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SOCAnalystLab
 This is an Azure Cloud Lab I have created in order to help new cybersecurity professionals gain hands on experience with cybersecurity topics.  This will build the lab, set up SIEM/SOAR open sourced applications and break down the cloud environment.
+Before using the associated script to build your Azure lab, you need to have Azure CLI installed as well as an Azure account with an Active Subscription.  
+Keep in Mind that you will be charged for resources you build using this script.  
+Have Fun!
